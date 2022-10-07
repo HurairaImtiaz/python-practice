64f7ab1248d0df96e718cbe361185c55b57cfd11
Huraira Imtiaz <91866100+HurairaImtiaz@users.noreply.github.com>
2022-10-07 21:37:01 +0500

    list functions (Day4)
    append, clear, copy, count , extend, index , insert , pop , remove , reverse , sort.

---
+++ (Day4)lists and string functions.py
@@ -0,0 +1,19 @@
+# lists and string functions
+list1=["pen", "pencil", "marker","balpoint"]
+list1.append("eraser")
+print(list1)
+list1.clear()
+print(list1)
+list1.copy()
+print(list1)
+a =list1.count("pen")
+print (a)
+list1.extend("scale")
+print(list1)
+list2 =[1,2,3]
+list1.extend(list2)
+print(list1)
+list1.sort()
+print(list1)
+list2.reverse()
+print(list2)
