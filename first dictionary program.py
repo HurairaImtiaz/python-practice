13882f71208be9320bee0cb27135c0b2163d865f
Huraira Imtiaz <91866100+HurairaImtiaz@users.noreply.github.com>
2022-10-07 23:07:39 +0500

    Boys Nm

---
+++ first dictionary program.py
@@ -0,0 +1,13 @@
+# first dictionary program
+print("WELCOME TO THE MUSLIM BOYS NAMES DICTIONARY")
+
+print("HERE YOU CAN FIND THE MEANING OF YOUR NAME ")
+d1={"Muhammad": "praiseworthy..قابل تعریف","Aabid":"One who worships","Aadil":"One who acts with justice and fairness", "Aariz":"Respectable and intelligent man",
+    "Ibrahim":"Father of a multitude (many)", "Adnan":"Settler or pioneer", "Ahmed": "Praiseworthy or commendable",
+    "Ali": "High, elevated, or champion", "Arham": "Most merciful", "Danish":"A Persian word that means knowledge or wisdom",
+    "Hammad": "Praiser", "Junaid" : "Soldier or warrior", "Ehtesham" : "Modesty, decency, Intelligent, judicious, learned, erudite",
+    "Ehtisham" : "Modesty, decency, Intelligent, judicious, learned, erudite", "Huraira":"Little Cat , Kitten", "Abu Huraira":"Father of cats"
+    }
+print("Enter Your Name :")
+a=input()
+print("The meaning of your name is ",d1[a])
